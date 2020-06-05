8cb3a4cb964d2537c36df4de83cfca8a4079e697
xrakennek <47826912+xrakennek@users.noreply.github.com>
2020-06-05 22:13:33 +0800

    2020-11111

--- script/bs_export.js
+++
@@ -1,5 +0,0 @@
-export const myNumber=[1,2,3,4];
-export function timer_countdown(target){
-  let td=new Date(target).getTime();
-  return td;
-}
--- script/bs_import.js
+++
@@ -1,2 +0,0 @@
-import * as Tryme from './bs_export.js';
-console.log(Tryme);
