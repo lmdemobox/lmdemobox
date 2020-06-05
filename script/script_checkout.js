872eef01a3957274748196b48c322d4116130ca9
xrakennek <47826912+xrakennek@users.noreply.github.com>
2020-06-05 21:30:58 +0800

    2020-06-05

---
+++ script/script_checkout.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  document.title='Checkout | Linkedmarts';
+
+
+  $('#position_nav').load('component/nav.html');
+  $('#position_cat').load('component/categoryBar.html');
+  $('#checkoutContent_position').load('component/checkoutContent.html');
+
+  $('#position_footer').load('component/footer.html');
+
+
+
+});
---
+++ script/script_myOrders.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  document.title='我的訂單 | Linkedmarts';
+
+
+  $('#position_nav').load('component/nav.html');
+  $('#position_cat').load('component/categoryBar.html');
+  $('#position_myOrders').load('component/myorderContent.html');
+
+  $('#position_footer').load('component/footer.html');
+
+
+
+});
